c88121e55c8631e608f62ac41b746065f0df54dc
qwx <qwx@sciops.net>
2026-01-07 10:28:08 +0000

    jamterm: don't spin at 100% cpu while holding mmb on scrollbar

--- sys/src/cmd/jamterm/scroll.c
+++ sys/src/cmd/jamterm/scroll.c
@@ -104,6 +104,8 @@ scroll(Flayer *l, int but)
 			o = (tot / (s.max.y - s.min.y)) * my;
 			n = 0;
 			forcenter(l, o, n);
+			if(readmouse(mousectl) < 0)
+				panic("mouse");
 		}else{
 			o = l->origin;
 			n = my/l->f.font->height;
