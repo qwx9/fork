ed96953b51f0f05d641817824f7ca2a972cd4abd
qwx <qwx@sciops.net>
2026-04-30 00:02:42 +0000

    git: sync with upstream

--- sys/src/cmd/git/get.c
+++ sys/src/cmd/git/get.c
@@ -270,7 +270,8 @@ fetchpack(Conn *c)
 		}
 		first = 0;
 
-		getfields(buf, sp, nelem(sp), 1, " \t\n\r");
+		if(getfields(buf, sp, nelem(sp), 1, " \t\n\r") < 2)
+			sysfatal("invalid ref line");
 		if(strstr(sp[1], "^{}"))
 			continue;
 		if(!okrefname(sp[1]))
@@ -305,10 +306,10 @@ fetchpack(Conn *c)
 	fmtcaps(c, caps, sizeof(caps));
 	for(i = 0; i < nref; i++){
 		if(hasheq(&have[i], &want[i]))
-			continue;
+			goto skip;
 		for(j = 0; j < i; j++)
 			if(hasheq(&want[i], &want[j]))
-				continue;
+				goto skip;
 		if((o = readobject(want[i])) != nil){
 			unref(o);
 			continue;
@@ -317,6 +318,7 @@ fetchpack(Conn *c)
 			sysfatal("could not send want for %H", want[i]);
 		caps[0] = 0;
 		req = 1;
+skip:;
 	}
 	flushpkt(c);
 
@@ -509,7 +511,7 @@ main(int argc, char **argv)
 		break;
 	}ARGEND;
 
-	gitinit();
+	gitinit(nil, 0, nil);
 	if(argc != 1)
 		usage();
 
