672b0f55e9b6b1f6758706c3ab3b7393d3310ecc
qwx <qwx@sciops.net>
2026-07-02 05:03:50 +0000

    nusb/joy and mkfile: sync with 9front

--- sys/src/cmd/nusb/joy/joy.c
+++ sys/src/cmd/nusb/joy/joy.c
@@ -516,8 +516,9 @@ quirks(KDev *kd)
 	if(d->usb->vid == 0x045e && d->usb->did == 0x028e
 	|| d->usb->vid == 0x0f0d && d->usb->did == 0x00ed
 	|| d->usb->vid == 0x1bad && d->usb->did == 0xf03a
-	|| d->usb->vid == 0x2dc8 && d->usb->did == 0x310a)
+	|| d->usb->vid == 0x2dc8 && d->usb->did == 0x310a){
 		xbox360(kd);
+	}
 }
 
 static void
